1e824d1090bf834b936ea47d74a0900ef56e8d6d
André Alves <andresalves2001@gmail.com>
2024-10-03 08:15:10 -0300

    GUI finalized

--- Assets/Input/InputManager.cs
+++ Assets/Input/InputManager.cs
@@ -1,20 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
-    //definição de variáveis
+    public static bool isPaused;
+
     public static Vector2 Movement;
     public static Vector2 MousePosition;
+
     private PlayerInput _playerInput;
+
     private InputAction _moveAction;
-
     private InputAction _fireAction;
-
     private InputAction _aimHorizontal;
     private InputAction _aimVertical;
+
+    private InputAction _pauseUnpause;
+    [SerializeField] public GameObject pauseScreen;
+
     public static event System.Action OnFire;
     private void Awake()
     {
@@ -28,18 +34,49 @@ public class InputManager : MonoBehaviour
         _aimHorizontal = _playerInput.actions["AimHorizontal"];
         _aimVertical = _playerInput.actions["AimVertical"];
 
+        isPaused = false;
+        _pauseUnpause = _playerInput.actions["PauseUnpause"];
+        pauseScreen.SetActive(false);
+
         //Ativa a função de disparar quando recebe o input 
         _fireAction.performed += Fire;
 
     }
     private void Update(){
-        //movimentação do player e posição do mouse é lida
+        isPaused = _pauseUnpause.WasPerformedThisFrame() ? !isPaused : isPaused;
+        if (!isPaused)
+        {
+            Unpause();
+
+        }
+        else
+        {
+            Pause();
+        }
+
+        //movimentação do player e posição do mouse é lida;
         Movement = _moveAction.ReadValue<Vector2>();
         MousePosition = new Vector2(_aimHorizontal.ReadValue<float>(), _aimVertical.ReadValue<float>());
     }
-//Ativação da função de tiro no PlayerCtrl
+
+    //Ativação da função de tiro no PlayerCtrl
     private void Fire(InputAction.CallbackContext context){
+        if (isPaused) { return; }
         OnFire?.Invoke();
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        Time.timeScale = 0.0f;
+        pauseScreen.SetActive(true);
+    }
+
+    public void Unpause()
+    {
+        isPaused = false;
+        Time.timeScale = 1.0f;
+        pauseScreen.SetActive(false);
     }
 }
    
